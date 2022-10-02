28f0d30aece5d281b6a3127c9a0eb6fb3729e667
alo.vahtre <alo.vahtre@quretec.com>
2022-10-02 19:31:00 +0300

    added script and login funciton button

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+function manage(txt) {
+    var bt = document.getElementById('submit');
+    if (txt.value != '') {
+        bt.disabled = false;
+    }
+    else {
+        bt.disabled = true;
+    }
+}
+
+
+
