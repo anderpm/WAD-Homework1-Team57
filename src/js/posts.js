a1d39e292184ae9e1898578f52c33a87c3cb5905
anderpm <6pander6@gmail.com>
2022-10-29 01:34:40 +0300

    Added json and fetch the posts information

---
+++ src/js/posts.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+
+    fetch('http://myjson.dit.upm.es/api/bins/782a')
+        .then((response) => response.json())
+        .then(json => {
+                        
+            for (let i = 0; i < json.length; i++) {
+                let date = document.getElementsByClassName("date")
+                date[i].innerHTML = json[i].createTime
+
+                let comment = document.getElementsByClassName("comment")
+                comment[i].innerHTML = json[i].comment
+
+                let photo = document.getElementsByClassName("photo")
+                photo[i].src = json[i].photo
+            }
+            
+        })
+        .catch(err => {
+
+        })
+        .finally(() => {
+
+        })
+        
+}
