7b04246a48b414938d93b7404484daf97c028e56
Vahtre <alovahtre@hotmail.com>
2022-10-30 21:25:20 +0200

    dropdown menu

--- src/js/script.js
+++ src/js/script.js
@@ -8,5 +8,22 @@ function manage(txt) {
     }
 }
 
+//function for showing dropdown menu
+function toggleDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
 
+//function for not showing dropdown menu
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
 
