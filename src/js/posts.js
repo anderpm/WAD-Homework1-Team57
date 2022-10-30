19a73b905704d37c200edad8b3c9860aa8e47801
Aritz <aritzbasurco@gmail.com>
2022-10-30 19:34:23 +0200

    I added the 10 post and I made the 5 point

--- src/js/posts.js
+++ src/js/posts.js
@@ -1,6 +1,31 @@
+fetch("./res/json/posts.json")
+.then((response) => response.json())
+.then(json => {
+                
+    for (let i = 0; i < json.length; i++) {
+        let date = document.getElementsByClassName("date")
+        date[i].innerHTML = json[i].createTime
+
+        let comment = document.getElementsByClassName("comment")
+        comment[i].innerHTML = json[i].comment
+
+        let photo = document.getElementsByClassName("photo")
+        photo[i].src = json[i].photo
+    }
+    
+})
+.catch(err => {
+
+})
+.finally(() => {
+
+})
+
+
+/*
 window.onload = function() {
 
-    fetch('http://myjson.dit.upm.es/api/bins/782a')
+    fetch('http://myjson.dit.upm.es/api/bins/57ly')
         .then((response) => response.json())
         .then(json => {
                         
@@ -22,5 +47,6 @@ window.onload = function() {
         .finally(() => {
 
         })
-        
+   
 }
+        */
